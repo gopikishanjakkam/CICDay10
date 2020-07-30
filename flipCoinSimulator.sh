79bda423864c48bea14001e0f8dd41756cc08753
Gopi <gopikishanjakkam@gmail.com>
2020-07-30 23:16:19 +0530

    add loop in flip coin simulator file

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,9 +1,20 @@
 #!/bin/bash -x
 
-randomcheck=$(($RANDOM%2))
-if [ $randomcheck -eq 0 ]
-then
-    echo Heads
-else
-    echo tails
-fi
+head=0
+tails=0
+
+for((i=0;i<10;i++))
+do
+  randomcheck=$(($RANDOM%2))
+  if [ $randomcheck -eq 0 ]
+  then
+      echo Heads
+      ((head++))
+  else
+      echo Tails
+      ((tails++))
+  fi
+done
+
+echo "Heads occured $head times"
+echo "Tails occured $tails times"
