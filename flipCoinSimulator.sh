4bd5dda159b5284d5bde7ffd4712bb9be1bfe7e8
Gopi <gopikishanjakkam@gmail.com>
2020-07-31 16:28:00 +0530

    add what to do if its tie

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -16,6 +16,22 @@ do
   fi
 done
 
+if [ $head -eq $tails ]
+then
+    while [[ $(($head-$tails)) -lt 2 && $(($tails-$head)) -lt 2 ]]
+    do
+      randomcheck=$(($RANDOM%2))
+      if [ $randomcheck -eq 0 ]
+      then
+          ((head++))
+          echo
+      else
+          echo Tails
+          ((tails++))
+      fi
+    done
+fi
+
 echo "Heads occured $head times"
 echo "Tails occured $tails times"
 
@@ -25,6 +41,4 @@ then
 elif [ $tails -gt $head ]
 then
     echo "Tails wins"
-else
-    echo "tie"
 fi
