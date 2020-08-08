4df8e503bb09904e88fd572a9c42f4bfc42a6743
Gopi <gopikishanjakkam@gmail.com>
2020-08-08 09:55:40 +0530

    add singlet

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,9 +1,22 @@
 #!/bin/bash -x
 
-randomcheck=$(($RANDOM%2));
-if [ $randomcheck -eq 0 ];
-then
-    echo Heads
-else
-    echo Tails
-fi
+n=15
+declare -A singlet
+singlet[H]=0
+singlet[T]=0
+
+for((i=1;i<=$n;i++))
+do
+  randomcheck=$(($RANDOM%2));
+  if [ $randomcheck -eq 0 ];
+  then
+      echo Heads
+      singlet[H]=$((${singlet[H]}+1));
+  else
+      echo Tails
+      singlet[T]=$((${singlet[T]}+1));
+  fi
+done
+
+echo "percentage of H="$((${singlet[H]}*100/$n))
+echo "percentage of T="$((${singlet[T]}*100/$n))
